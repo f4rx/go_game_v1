887d2ca301d7fe561be0893ed57fd465c6a70d72
Aleksey Stepanenko <f1ex@inbox.ru>
2018-12-08 18:35:26 +0300

    init commit

---
+++ game.go
@@ -0,0 +1,9 @@
+package main
+
+import(
+	"fmt"
+)
+
+func printText(s string){
+	fmt.Println(s)
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import(
+	"first_game/game"
+)
+
+func main(){
+	printText("main funct")
+}
