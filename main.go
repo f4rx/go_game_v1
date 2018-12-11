e328149153bd8b0511f5a10973ab4f1bc2211b6d
Aleksey Stepanenko <f1ex@inbox.ru>
2018-12-11 11:31:48 +0300

    fix up move and gofmt

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
-import(
-	"fmt"
+import (
 	"errors"
+	"fmt"
 )
 
 type Player struct {
@@ -14,15 +14,23 @@ type Position struct {
 	Y int
 }
 
+func (p *Position) addX(i int) {
+	p.X += i
+}
+
+func (p *Position) addY(i int) {
+	p.Y += i
+}
+
 type GameMap struct {
-	gameMap [][]int
+	gameMap   [][]int
 	positions map[*Player]Position
 }
 
 func createGameMap(size int, positions map[*Player]Position) GameMap {
 	var gameMapArr = make([][]int, size)
 	for i := range gameMapArr {
-    	gameMapArr[i] = make([]int, size)
+		gameMapArr[i] = make([]int, size)
 	}
 	gameMap := GameMap{gameMapArr, positions}
 	return gameMap
@@ -32,13 +40,15 @@ func (g *GameMap) movePlayer(p *Player, direction string) error {
 	switch direction {
 	case "left":
 		fmt.Println("move left")
-		g.positions[p].X -= 1
 	case "right":
 		fmt.Println("move right")
 	case "down":
 		fmt.Println("move down")
 	case "up":
 		fmt.Println("move up")
+		playerPosition := g.positions[p]
+		playerPosition.X--
+		g.positions[p] = playerPosition
 	default:
 		return errors.New("Непрделенное движение, " + direction)
 	}
@@ -49,15 +59,16 @@ func (g *GameMap) print() {
 	fmt.Println(*g)
 }
 
-func main(){
+func main() {
 	fmt.Println("fmt mock")
-	player1 := Player{}
+	player1 := Player{"player1"}
 	player2 := new(Player)
 	positions := make(map[*Player]Position)
-	positions[&player1] = Position{2,2}
-	positions[player2] = Position{1,1}
+	positions[&player1] = Position{2, 2}
+	positions[player2] = Position{1, 1}
 	gameMap := createGameMap(5, positions)
 	gameMap.print()
 	gameMap.movePlayer(&player1, "up")
 	gameMap.movePlayer(player2, "down")
+	gameMap.print()
 }
